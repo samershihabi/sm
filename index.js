ebd075f4cef25b74e285f601fd282e5eadbd1b5d
saleh-jaz1@hotmail.com <P@$$w0rd>
2018-04-12 09:53:16 +0300

    update callbacks

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-var kvm = require('./lib/requests/apigee-proxy-kvm-apis');
-var util = require('./lib/utils');
-var apis = require('./lib/requests/apigee-api-proxy-apis');
-var ziputils = require('./lib/ziputils');
 
-//var dirCheck=util.apiProxyDirsCheck('/Users/ap/ab_api_proxies/apiproxy/');
-ziputils.zipDirectory('/Users/ap/ab_api_proxies/mock/', 'apiproxy', function (err, zipBuf) {
-    if (err) throw new Error(err);
-    console.log(zipBuf)
-    //apis.importNewApiProxy("saleh-jaz1@hotmail.com", "P@$$w0rd", "salehmashal", "mock",zipBuf);
-    apis.deployApiProxyRevision("saleh-jaz1@hotmail.com", "P@$$w0rd", "salehmashal", "test", "mock", 1, 100, true);
-});
+
+
+
+
+
